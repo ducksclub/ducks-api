929e7f0b3284e9f9f9663acf56647d57fe14ae88
Maulen <azikulovv@gmail.com>
2026-06-27 15:02:27 +0500

    style(eslint): унифицирован стиль и форматирование цитат в конфигурации ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,20 @@
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 
 export default [
   {
-    ignores: ["dist", "node_modules"]
+    ignores: ['dist', 'node_modules'],
   },
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     languageOptions: {
       parser: tsParser,
-      parserOptions: { project: "./tsconfig.json" }
+      parserOptions: { project: './tsconfig.json' },
     },
-    plugins: { "@typescript-eslint": tseslint },
+    plugins: { '@typescript-eslint': tseslint },
     rules: {
       ...tseslint.configs.recommended.rules,
-      "@typescript-eslint/no-explicit-any": "warn"
-    }
-  }
-];
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+]
